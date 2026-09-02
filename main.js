eef979f5e70e210e9e26b3fd404879c369cf3b0e
profeLeguizaNicolas <136519391+profeLeguizaNicolas@users.noreply.github.com>
2026-09-02 15:56:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const boton = document.querySelector(".botonEjemplo");
 const texto = document.querySelector(".textoEjemplo");
 
 boton.addEventListener('click', ()=>{
-    boton.style.width = `${variable}px`;
+  
 texto.style.color = 'red';
 });
